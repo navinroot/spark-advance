ac2f2c5656dde49cfaa414fff0702cd4a28a1ea5
navinroot <navinaryan6@gmail.com>
2019-08-20 13:19:15 +0530

    adding some more code
    Signed-off-by: navinroot <navinaryan6@gmail.com>

--- src/main/scala/com/test/scala_traning/MapOperation.scala
+++ src/main/scala/com/test/scala_traning/MapOperation.scala
@@ -5,13 +5,13 @@ import scala.collection._
 object MapOperation extends App {
 
   val m1 = immutable.Map("Megha" -> 3, "Ruchi" -> 2, "Becky" -> 4)
+  var m = immutable.Map("Ayushi" -> 0, "Megha" -> 4)
   /**
    * immutable map contains only unique keys
    *
    */
 
   val m2 = m ++ m1
-  val mm = mutable.Map("Ayushi" -> 0, "Megha" -> 4)
 
   //  m1 += ("navin" -> 4)  [ NOT POSSIBLE BECAUSE OF VAL M1]
   m += ("navin" -> 3)
@@ -32,12 +32,7 @@ object MapOperation extends App {
    *
    */
   println("mutable map started")
-  /**
-   * synchronized mutable thread safe map
-   *
-   */
-
-  val sychronizedMap = new mutable.HashMap[String, Int]() with mutable.SynchronizedMap[String, Int]
+  val mm = mutable.Map("Ayushi" -> 0, "Megha" -> 4)
 
   // add new element to same mutable map
 
@@ -60,7 +55,14 @@ object MapOperation extends App {
 
   // clear all the element for mutable map
   mm.clear()
-  var m = immutable.Map("Ayushi" -> 0, "Megha" -> 4)
+
+
+  /**
+   * synchronized mutable thread safe map
+   *
+   */
+
+  val sychronizedMap = new mutable.HashMap[String, Int]() with mutable.SynchronizedMap[String, Int]
   sychronizedMap.put("navin", 1)
   println(sychronizedMap.mkString(" "))
 
